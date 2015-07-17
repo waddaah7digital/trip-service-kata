d6b1ed994e71ecbdb77dfa17f76af7e4c378608e
Waddaah Abdu <waddaah.abdu@7digital.com>
2015-07-17 16:37:07 +0100

    Tidying

---
+++ c#/TripServiceKata/TripServiceKata.Tests/MockTripDAOWrapper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using TripServiceKata.Trip;
+
+namespace TripServiceKata.Tests
+{
+	public class MockTripDAOWrapper : ITripDAOWrapper
+	{
+		private readonly List<Trip.Trip> _trips;
+
+		public MockTripDAOWrapper(List<Trip.Trip> trips)
+		{
+			_trips = trips;
+		}
+
+		public List<Trip.Trip> FindTripsByUser(User.User user)
+		{
+			return _trips;
+		}
+	}
+}
--- c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
+++ c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using NUnit.Framework;
 using TripServiceKata.Exception;
 using TripServiceKata.Trip;
-using TripServiceKata.User;
 
 namespace TripServiceKata.Tests
 {
@@ -13,16 +12,16 @@ namespace TripServiceKata.Tests
 		[Test]
 		public void When_no_user_is_logged_in_throws_user_not_logged_in_exception()
 		{
-			var userSession = new MockUserSession(null);
-			var tripService = new TripService(userSession, new TripDAOWrapper());
+			var tripService = new TripService(new MockUserSession(null), new MockTripDAOWrapper(new List<Trip.Trip>()));
+			
 			Assert.Throws<UserNotLoggedInException>(() => tripService.GetTripsByUser(new User.User()));
 		}
 
 		[Test]
 		public void When_logged_in_user_is_not_a_friend_then_returns_no_trips()
 		{
-			var userSession = new MockUserSession(new User.User());
-			var tripService = new TripService(userSession, new TripDAOWrapper());
+			var tripService = new TripService(new MockUserSession(new User.User()), new MockTripDAOWrapper(new List<Trip.Trip>()));
+			
 			var actual = tripService.GetTripsByUser(new User.User());
 
 			Assert.That(actual, Is.EqualTo(Enumerable.Empty<Trip.Trip>()));
@@ -32,29 +31,12 @@ namespace TripServiceKata.Tests
 		public void When_logged_in_user_is_a_friend_then_returns_trips_from_service()
 		{
 			var loggedInUser = new User.User();
-			var userSession = new MockUserSession(loggedInUser);
-			var tripsFromService = new List<Trip.Trip>{new Trip.Trip()};
-			var tripService = new TripService(userSession, new MockTripDAOWrapper(tripsFromService));
+			var tripService = new TripService(new MockUserSession(loggedInUser), new MockTripDAOWrapper(new List<Trip.Trip>{new Trip.Trip()}));
 			var userToQuery = new User.User();
 			userToQuery.AddFriend(loggedInUser);
 
 			var actual = tripService.GetTripsByUser(userToQuery);
-			Assert.That(actual, Is.EqualTo(tripsFromService));
-		}
-	}
-
-	public class MockTripDAOWrapper : ITripDAOWrapper
-	{
-		private readonly List<Trip.Trip> _trips;
-
-		public MockTripDAOWrapper(List<Trip.Trip> trips)
-		{
-			_trips = trips;
-		}
-
-		public List<Trip.Trip> FindTripsByUser(User.User user)
-		{
-			return _trips;
+			Assert.That(actual, Is.EqualTo(new List<Trip.Trip>{new Trip.Trip()}));
 		}
 	}
 }
